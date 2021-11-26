348c77a6d6c72ba262a95637e336344fd88894bc
unknown <rakeshsmusale03@gmail.com>
2021-11-26 14:06:57 -0800

    UC5 Retrive salary from particular employee.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,6 +10,20 @@ salary float,
 startDate date
 );
 
+---------------------UC4 Retrive Data into Table---------
 select * from employee_payroll;
-insert into employee_payroll values ('Rakesh',5000,'2019-03-01'),('vishal',8000,'2019-07-11');
 
+---------------------UC3 Insert Table----------------------
+insert into employee_payroll values ('Rakesh',5000,'2019-03-01');
+insert into employee_payroll values('shital',23000.20,'2-09-2016');
+insert into employee_payroll values('Bill',12000.33,'2-11-2018');
+insert into employee_payroll values('Meena',5500.200,'1-03-2019');
+insert into employee_payroll values('Charli',456123,'2-02-2020');
+
+---------------------UC5 Retrive Data into Table---------
+
+select * from employee_payroll where name='Meena';
+
+select *from employee_payroll where startDate between CAST('2018-03-01 ' as date) and GETDATE();
+
+select salary from employee_payroll where id=4;
