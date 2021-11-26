73e592cbfc4f18ac0c9ed66fbc2a20c864ead5f5
unknown <rakeshsmusale03@gmail.com>
2021-11-26 13:17:51 -0800

    UC2 create table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,3 +2,10 @@
 create database payroll_service;
  
  use payroll_service;
+ --------UC2 Create Table----------
+create table employee_payroll(
+id int Identity(1,1) primary key,
+name varchar(200),
+salary float,
+startDate date
+);
